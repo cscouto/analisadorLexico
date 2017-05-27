8cbe8daa076fc8cbdac13634fb28c7032c6a0a9d
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-05-27 20:57:59 -0300

    metodos

--- src/AnSintatico.java
+++ src/AnSintatico.java
@@ -63,13 +63,20 @@ public class AnSintatico {
 				// TODO tratar erro
 			}
 		} else {
+			bufferToken = t;
 			processaCMD();
 		}
 	}
 
 	// DECL | COND | REP | ATRIB
 	private void processaCMD() {
-		Token t = lexico.nextToken();
+		Token t;
+		if (bufferToken != null) {
+			t = bufferToken;
+			bufferToken = null;
+		} else {
+			t = lexico.nextToken();
+		}
 		bufferToken = t;
 		switch (t.getTokenCode()) {
 		case WHILE:
@@ -136,7 +143,6 @@ public class AnSintatico {
 		switch(t.getTokenCode()){
 		case ELSE:
 			//TODO valida token
-			t = lexico.nextToken();
 			processaBLOCO();
 			break;
 		case WHILE:
@@ -180,8 +186,7 @@ public class AnSintatico {
 			//TODO valida token
 			t = lexico.nextToken();
 			if (t.getTokenCode() == TokenID.ATTRIB_OP){
-				//TODO valida token
-				t = lexico.nextToken();
+				//TODO valida token;
 				processaATRIB2();
 				t = lexico.nextToken();
 				if (t.getTokenCode() == TokenID.TERM){
@@ -234,40 +239,76 @@ public class AnSintatico {
 		}
 	}
 
-	//r_par AT_VN2 /while AT_VN2 /to /id AT_VN2	/for AT_VN2 /multdiv_op EXP_N  AT_VN2 /addsub_op EXP_N  AT_VN2 /rel_op AT_VN2 /term /if  AT_VN2 /declare AT_VN2 /begin AT_VN2
+	//
 	private void processaATR_VN() {
+		processaEXP_N2();
+		processaAT_VN2();
+	}
+	
+	//rel_op EXP_N | term
+	private void processaAT_VN2() {
+		Token t = lexico.nextToken();
+		if (t.getTokenCode() == TokenID.REL_OP){
+			//TODO valida token
+			processaEXP_N();
+		}else if (t.getTokenCode() == TokenID.TERM){
+			//TODO valida token
+		}else{
+			//TODO erro
+		}
+	}
+
+	//r_par | while | to | id | for | multdiv_op EXP_N | addsub_op EXP_N | rel_op |	term | if |	declare | begin 
+	private void processaEXP_N2() {
 		Token t =  lexico.nextToken();
 		switch(t.getTokenCode()){
 			case R_PAR:
+				//TODO valida token
 				break;
 			case WHILE:
+				//TODO valida token
 				break;
 			case TO:
+				//TODO valida token
 				break;
 			case ID:
+				//TODO valida token
 				break;
 			case FOR:
+				//TODO valida token
 				break;
 			case MULTDIV_OP:
+				//TODO valida token
+				bufferToken = t;
+				processaEXP_N();
 				break;
 			case ADDSUB_OP:
+				//TODO valida token
+				bufferToken = t;
+				processaEXP_N();
 				break;
 			case REL_OP:
+				//TODO valida token
 				break;
 			case TERM:
+				//TODO valida token
 				break;
 			case IF:
+				//TODO valida token
 				break;
 			case DECLARE:
+				//TODO valida token
 				break;
 			case BEGIN:
+				//TODO valida token
 				break;
 			default:
+				//TODO ERROR
 				break;
 		}
-		
 	}
-
+	
+	//num_int | num_float
 	private void processaVAL_N() {
 		Token t;
 		if (bufferToken != null) {
@@ -276,19 +317,92 @@ public class AnSintatico {
 		} else {
 			t = lexico.nextToken();
 		}
-		
+		if (t.getTokenCode() == TokenID.NUM_FLOAT){
+			//TODO valida token
+		}else if (t.getTokenCode() == TokenID.NUM_FLOAT){
+			//TODO valida token
+		}else{
+			//TODO error
+		}
 	}
-
+	
+	//expL2
 	private void processaATR_ID() {
-		// TODO Auto-generated method stub
-		
+		processaEXP_L2();
+	}
+	
+	//rel_op EXP_N | EXP_L3
+	private void processaEXP_L2() {
+		Token t = lexico.nextToken();
+		if (t.getTokenCode() == TokenID.REL_OP){
+			//TODO valida token
+			processaEXP_N();
+		}else{
+			bufferToken = t;
+			processaEXP_L3();
+		}
 	}
 
+	//r_par | logic_op EXP_L | term 
+	private void processaEXP_L3() {
+		Token t;
+		if (bufferToken != null) {
+			t = bufferToken;
+			bufferToken = null;
+		} else {
+			t = lexico.nextToken();
+		}
+		if (t.getTokenCode() == TokenID.R_PAR){
+			//TODO valida token
+		}else if (t.getTokenCode() == TokenID.LOGIC_OP){
+			//TODO valida token
+			processaEXP_L();
+		}else if (t.getTokenCode() == TokenID.TERM){
+			//TODO valida token
+		}else{
+			//TODO error
+		}
+	}
+	//l_par EXP_N r_par | id | num_float EXP_N2 | num_int EXP_N2
 	private void processaEXP_N() {
-		// TODO Auto-generated method stub
-		
+		Token t;
+		if (bufferToken != null) {
+			t = bufferToken;
+			bufferToken = null;
+		} else {
+			t = lexico.nextToken();
+		}
+		switch(t.getTokenCode()){
+			case L_PAR:
+				//TODO valida token
+				processaEXP_N();
+				t = lexico.nextToken();
+				if (t.getTokenCode()==TokenID.R_PAR){
+					//TODO valida token
+				}else{
+					//TODO error
+				}
+				break;
+			case ID:
+				//TODO valida token
+				break;
+			case NUM_FLOAT:
+				bufferToken = t;
+				processaVAL_N();
+				processaEXP_N2();
+				break;
+			case NUM_INT:
+				bufferToken = t;
+				processaVAL_N();
+				processaEXP_N2();
+				break;
+			default:
+				//TODO error
+				break;
+		}
 	}
 
+	//declare id type term 
 	private void processaDECL() {
 		Token t;
 		if (bufferToken != null) {
@@ -297,8 +411,30 @@ public class AnSintatico {
 		} else {
 			t = lexico.nextToken();
 		}
+		if (t.getTokenCode() == TokenID.DECLARE){
+			//TODO valida token
+			t = lexico.nextToken();
+			if (t.getTokenCode() == TokenID.ID){
+				//TODO valida token
+				t = lexico.nextToken();
+				if (t.getTokenCode() == TokenID.TYPE){
+					//TODO valida token
+					t = lexico.nextToken();
+					if (t.getTokenCode() == TokenID.TERM){
+						//TODO valida token
+					}else{
+						//TODO error
+					}
+				}else{
+					//TODO error
+				}
+			}else{
+				//TODO error
+			}
+		}else{
+			//TODO error
+		}
 	}
-
 	private void processaREP() {
 		Token t;
 		if (bufferToken != null) {
@@ -308,14 +444,10 @@ public class AnSintatico {
 			t = lexico.nextToken();
 		}
 		if (t.getTokenCode() == TokenID.FOR) {
-			if (bufferToken == null) {
-				bufferToken = t;
-			}
+			bufferToken = t;
 			processaREPF();
 		} else if (t.getTokenCode() == TokenID.WHILE) {
-			if (bufferToken == null) {
-				bufferToken = t;
-			}
+			bufferToken = t;	
 			processaREPW();
 		} else {
 			// TODO ERROR
@@ -352,11 +484,59 @@ public class AnSintatico {
 		}
 	}
 
+	//id EXP_L2 | num_float EXP_N2 rel_op EXP_N | num_int EXP_N2 rel_op EXP_N | logic_val EXP_L2 |  l_par EXP_N r_par 
 	private void processaEXP_L() {
-		// TODO Auto-generated method stub
-
+		Token t = lexico.nextToken();
+		switch(t.getTokenCode()){
+			case ID:
+				//TODO valida token
+				processaEXP_L2();
+				break;
+			case NUM_FLOAT:
+				bufferToken = t;
+				processaVAL_N();
+				processaEXP_N2();
+				t = lexico.nextToken();
+				if (t.getTokenCode() == TokenID.REL_OP){
+					//TODO valida token
+					processaEXP_N();
+				}else{
+					//TODO error
+				}
+				break;
+			case NUM_INT:
+				bufferToken = t;
+				processaVAL_N();
+				processaEXP_N2();
+				t = lexico.nextToken();
+				if (t.getTokenCode() == TokenID.REL_OP){
+					//TODO valida token
+					processaEXP_N();
+				}else{
+					//TODO error
+				}
+				break;
+			case LOGIC_VALUE:
+				//TODO valida token 
+				processaEXP_L2();
+				break;
+			case L_PAR:
+				//TODO valida token
+				processaEXP_N();
+				t = lexico.nextToken();
+				if (t.getTokenCode() == TokenID.R_PAR){
+					//TODO valida dado
+				}else{
+					//TODO error
+				}
+				break;
+			default:
+				//TODO error
+				break;
+		}
 	}
-
+	
+	//for id attrib_op EXP_N to EXP_N BLOCO
 	private void processaREPF() {
 		Token t;
 		if (bufferToken == null) {
